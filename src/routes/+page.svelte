9aecf2163c4be295d175ea893f1d4b6d52245105
fathiabdelmalek <abdelmalek.fathi.2001@gmail.com>
2026-04-06 14:34:38 +0100

    fix: remove unnecessary loading delays and skeleton loaders
    Static site doesn't need loading states - data is already available at build time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,24 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import Hero from "../components/Hero.svelte";
   import Projects from "../components/Projects.svelte";
   import Publications from "../components/Publications.svelte";
   import Skills from "../components/Skills.svelte";
   import About from "../components/About.svelte";
   import Contact from "../components/Contact.svelte";
-  import SkeletonLoader from "../components/SkeletonLoader.svelte";
   import { projects as projectsData } from "$lib/data/projects";
   import { skillCategories as skillsData } from "$lib/data/skills";
   import { publications as publicationsData } from "$lib/data/publications";
   
-  let projects = $state(projectsData);
-  let skillCategories = $state(skillsData);
-  let publications = $state(publicationsData);
-  let isLoading = $state(false);
-
-  onMount(() => {
-    isLoading = true;
-    setTimeout(() => {
-      isLoading = false;
-    }, 300);
-  });
+  let projects = projectsData;
+  let skillCategories = skillsData;
+  let publications = publicationsData;
 </script>
 
 <main>
   <Hero />
-  
-  {#if isLoading}
-    <section id="projects" class="py-32 px-5">
-      <div class="max-w-6xl mx-auto">
-        <SkeletonLoader type="project" />
-      </div>
-    </section>
-  {:else}
-    <Projects {projects} />
-  {/if}
-
+  <Projects {projects} />
   <Publications {publications} />
-
-  {#if isLoading}
-    <section id="skills" class="py-32 px-5">
-      <div class="max-w-6xl mx-auto">
-        <SkeletonLoader type="skill" />
-      </div>
-    </section>
-  {:else}
-    <Skills {skillCategories} />
-  {/if}
-  
+  <Skills {skillCategories} />
   <About />
   <Contact />
 </main>
